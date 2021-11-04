bc93522b171addda335e8d3f974473d2e51495f0
Andy Camann <acamann@gmail.com>
2021-11-04 07:05:11 -0500

    move colors back

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, StyleSheet, TextProps } from 'react-native';
-import { colors } from '../css/Colors';
+import { colors } from './Colors';
 
 const StyledText: React.FC<TextProps> = (props: TextProps) => {
   return (
